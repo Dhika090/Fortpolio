965f517b4ede7bbf0aa708e84b76dcb621152616
Andikabgspam_ <42061803+Dhika090@users.noreply.github.com>
2021-09-26 13:22:18 +0700

    Delete api directory

--- api/services.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import {services} from '../../data'
-
-export default (req:NextApiRequest,res:NextApiResponse) => {
-
-    //by deafult get  request
-    console.log(services)
-    res.status(200).json({services})
-}
